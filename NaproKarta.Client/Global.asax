542e6c02b9514bcc0b4a4eeae33d677b7a4fbdc1
Kuba Miszcz <zielonyeufor@gmail.com>
2017-12-05 20:17:18 +0100

    4

---
+++ NaproKarta.Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NaproKarta.Client.MvcApplication" Language="C#" %>
---
+++ NaproKarta.Client/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NaproKarta.Client.Startup))]
+namespace NaproKarta.Client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
